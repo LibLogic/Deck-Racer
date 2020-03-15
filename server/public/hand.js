c92d5015753d297202a342e6e50215b6ca15e1dc
Tom Hodgkinson <hdgknsn@gmail.com>
2020-03-15 18:56:52 -0400

    added hide address bar hack

--- server/public/hand.js
+++ server/public/hand.js
@@ -1,5 +1,23 @@
 let cs = new WebSocket("wss://finish-line-game.herokuapp.com");
 
+function hideAddressBar() {
+  if (!window.location.hash) {
+    if (document.height <= window.outerHeight + 10) {
+      document.body.style.height = window.outerHeight + 50 + "px";
+      setTimeout(function() {
+        window.scrollTo(0, 1);
+      }, 50);
+    } else {
+      setTimeout(function() {
+        window.scrollTo(0, 1);
+      }, 0);
+    }
+  }
+}
+
+window.addEventListener("load", hideAddressBar);
+window.addEventListener("orientationchange", hideAddressBar);
+
 cs.onmessage = e => {
   let obj = JSON.parse(e.data);
   switch (obj.type) {
